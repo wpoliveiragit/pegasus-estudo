c8a62c946ce04f46e4ab99989886f30309f23a20
wpoliveiracel <wp.oliveira.cel@gmail.com>
2024-11-20 07:25:34 -0300

    adicao do projeto base

---
+++ src/main/java/br/com/pegasus/esturdo/PegasusEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pegasus.esturdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegasusEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegasusEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pegasus/esturdo/PegasusEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pegasus.esturdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegasusEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
